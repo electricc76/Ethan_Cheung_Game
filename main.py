7bd09bf4bba043a527959a6ba1812d4cadadeb2a
electricc76 <echeung0808@gmail.com>
2024-10-02 10:58:26 -0700

    Added velocity to player

--- main.py
+++ main.py
@@ -2,31 +2,46 @@
 
 # import all necessary modules and libraries
 import pygame as pg
-
 from settings import *
 from sprites import *
 from random import randint
 
 # created a game class to instantiate later
 # it will have all the neccessary parts to run the game
+
+# used to organize parts used to create the game
 class Game:
+    # __init__ method: creates an instance of itself
+    # Sets things up such as the game window name, width and height, the time, and if it is running or not
     def __init__(self):
+        # initializes the ability to use pygame
         pg.init()
+        # used for sound
         pg.mixer.init()
+        # sets how big the window is in pixels
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        # names the game "Ethan's Game"
         pg.display.set_caption("Ethan's Game")
+        # creates a clock so we can set the framerate
         self.clock = pg.time.Clock()
+        # is the game running or not? Yes
         self.running = True
+
     def new(self):
+        # creates all_sprites group so we can batch update and render
+        # defines properties that can be seen in the game system
         self.all_sprites = pg.sprite.Group()
-        self.player = Player(self, 50,50)
-        # instantiated a mob
-        self.mob = Mob(self, 100,100)
+        # instantiated a mob and a player at x and y
+        self.player = Player(self, 1,1)
+        self.mob = Mob(self, 100, 100)
+        # makes new mobs and walls using a for loop
         for i in range(6):
             Mob(self, i*randint(0, 200), i * randint (0, 200))
-        for i in range(6):
-            Wall(self, i*TILESIZE,128)
+            Wall(self, i*TILESIZE, i*TILESIZE)
     
+    # if the game is running (self.running = TRUE), the methods are called. events() lists out any 
+    # events that happen at that time. update() updates everything about the game once per tick. draw() actually
+    # draws out the new updated screen
     def run(self):
         while self.running: 
             self.dt = self.clock.tick(FPS) / 1000
@@ -35,6 +50,8 @@ class Game:
             self.draw()
     
         # input
+    
+    # looks for any events, specifically looks for closing the game with 'x'
     def events(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -42,6 +59,7 @@ class Game:
                 
         # process
     def update(self):
+        # a list of all the sprites in the game, updates all of them at once
         self.all_sprites.update()
 
         # output
@@ -49,6 +67,7 @@ class Game:
         pass
 
         # output
+    # covers the whole screen with black, then paints the new, updated sprites back on
     def draw(self):
         self.screen.fill(BLACK)
         self.all_sprites.draw(self.screen)
@@ -56,8 +75,10 @@ class Game:
 
 
 
-
+# checks the file name and creates a game object based on the Game() class.
 if __name__ == "__main__":
     g = Game()
+    # create all game elements with the new method (not function)
     g.new()
+    # run the game
     g.run()
--- sprites.py
+++ sprites.py
@@ -1,7 +1,7 @@
 # this file was created by Ethan Cheung
 
 import pygame as pg
-from sprites import Sprite
+from pygame.sprite import Sprite
 from settings import *
 import random
 
@@ -13,33 +13,40 @@ class Player(Sprite):
         self.image = pg.Surface((TILESIZE,TILESIZE))
         self.rect = self.image.get_rect()
         self.image.fill(RED)
-        self.rect.x = x
-        self.rect.y = y
+        # self.rect.x = x
+        # self.rect.y = y
+        self.x = x * TILESIZE
+        self.y = y * TILESIZE
         self.speed = 10
+        self.vx, self.vy = 0, 0
     
     def get_keys(self):
         # Load anything pressed on the keyboard into the variable "keys"
         # Checks if any of the keys pressed = A S W or D. If so, it makes the if True.
-
+        # constantly listening to see if keys are pressed
         keys = pg.key.get_pressed()
         if keys[pg.K_w]:
-            self.rect.y -= self.speed
+            self.vy -= self.speed
         if keys[pg.K_a]:
-            self.rect.x -= self.speed
+            self.vx -= self.speed
         if keys[pg.K_s]:
-            self.rect.y += self.speed
+            self.vy += self.speed
         if keys[pg.K_d]:
-            self.rect.x += self.speed
+            self.vx += self.speed
     def update(self):
         self.get_keys()
+        self.x += self.vx * self.game.dt
+        self.y += self.vy * self.game.dt
+
+        self.rect.x = self.x
+        self.rect.y = self.y
         
 class Mob(Sprite):
     def __init__(self, game, x, y):
+        self.game = game
         self.groups = game.all_sprites
         Sprite.__init__(self, self.groups)
-        self.game = game
         self.image = pg.Surface((TILESIZE,TILESIZE))
-        self.image.fill(RED)
         self.rect = self.image.get_rect()
         self.image.fill(GREEN)
         self.rect.x = x
@@ -53,11 +60,6 @@ class Mob(Sprite):
             print("off the screen...")
             self.speed *= -1
             self.rect.y += 32
-        elif self.rect.colliderect(self.game.player):
-            self.speed *= -1
-        elif self.rect.colliderect(self):
-            self.speed *= -1
-
 
         # When it hits the end of the screen, it moves down
         # turns around and goes the other way
@@ -67,14 +69,14 @@ class Mob(Sprite):
 
 class Wall(Sprite):
     def __init__(self, game, x, y):
-        self.groups = game.all_sprites
         self.game = game
-        Sprite.__init__(self)
+        self.groups = game.all_sprites
+        Sprite.__init__(self, self.groups)
         self.image = pg.Surface((TILESIZE, TILESIZE))
         self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-    
+
     def update(self):
         pass
